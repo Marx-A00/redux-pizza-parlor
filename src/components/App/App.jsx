93a005a79e76ac1bb33c4a2dff46a716fb3afea0
Marcos Andrade <mnandrade1999@gmail.com>
2023-11-30 14:28:23 -0600

    made progress on display and app.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,56 +1,68 @@
-import React from 'react';
-import axios from 'axios';
-import {useEffect, useState} from 'react'
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-
   const [pizzaList, setPizzaList] = useState([]);
 
-  useEffect(() =>{
+  const cartItems = useSelector(store => store.cart);
+  
+ const totalCartPrice = Number(cartItems.reduce((prev, current) => prev + Number(current.price),0));
+ console.log("Cart",cartItems);
+ console.log(totalCartPrice);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
     getPizzas();
-  },[]);
+  }, []);
 
-  const getPizzas= () => {axios({
-    method: 'GET',
-    url:'/api/pizza'
-  })
-  .then((response)=>{
-    const fetchedPizzas = response.data;
-    setPizzaList(fetchedPizzas)
-    console.log(fetchedPizzas)
-
-  })
-  .catch((error)=>{
-    console.log('error on GET',error);
-  })
-}
-
+  const getPizzas = () => {
+    axios({
+      method: "GET",
+      url: "/api/pizza",
+    })
+      .then((response) => {
+        const fetchedPizzas = response.data;
+        setPizzaList(fetchedPizzas);
+        console.log(fetchedPizzas);
+      })
+      .catch((error) => {
+        console.log("error on GET", error);
+      });
+  };
+  const addPizzaToCart = (event) => {
+    console.log(event.target.parentElement);
+    console.log(event.target.value);
+    // dispatch({
+    //   type: `ADD_PIZZA`,
+    //   payload: event.target.value,
+    // });
+  };
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
+    <div className="App">
+      <header className="App-header">
+        {totalCartPrice}
+        <h1 className="App-title">Prime Pizza</h1>
       </header>
       <div className="container">
-
-        {pizzaList.map((pizza)=>{
-
-          return(
-          <div className="card" height={200} width={200}>
-            <p>{pizza.name}</p>
-            <p>{pizza.description}</p>
-            <p>{pizza.price}</p>
-            <button>Add to Cart</button>
-
-          </div>
-          )
+        {pizzaList.map((pizza) => {
+          return (
+            <div className="card" height={200} width={200}>
+              <p>{pizza.name}</p>
+              <p>{pizza.description}</p>
+              <p>{pizza.price}</p>
+              <button onClick={addPizzaToCart}>Add to Cart</button>
+            </div>
+          );
         })}
       </div>
-  
+
       {/* <img src='images/pizza_photo.png' /> */}
       {/* <p>Pizza is great.</p> */}
-  
     </div>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,18 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 
 // Be sure to replace this reducer! 🙂
-const someReducer = (state = [], action) => {
+const cart = (state = [], action) => {
+  if(action.type === 'ADD_PIZZA'){
+    const thePizzaWeDispatched = action.payload;
+    return [...state,thePizzaWeDispatched];
+  }
+  
   return state;
 }
 
 const store = createStore(
   combineReducers({
-    someReducer, // 👈 Be sure to replace this, too!
+    cart, // 👈 Be sure to replace this, too!
   }),
   applyMiddleware(logger),
 );
